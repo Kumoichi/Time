cedd542794c6f9d693b9f800f742171b9cbbb99a
Kumoichi <64859961+Kumoichi@users.noreply.github.com>
2022-05-10 08:05:06 -0700

    study time app

---
+++ app/src/main/java/com/example/timer/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.timer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private static final long START_TIME_IN_MILLIS = 600000;
+
+    private TextView mTextViewCountDown;
+    private Button mButtonStartPause;
+    private Button mButtonReset;
+    private CountDownTimer mCountDownTimer;
+    private boolean mTimeRunning;
+
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextViewCountDown = findViewById(R.id.text_view_countdown);
+        mButtonStartPause = findViewById(R.id.button_start_pause);
+        mButtonReset = findViewById(R.id.button_reset);
+
+        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mTimeRunning){
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
+            }
+        });
+
+        mButtonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetTimer();
+            }
+        });
+        updateCountDownText();
+    }
+
+    private void startTimer()
+    {
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimeRunning = false;
+                updateButtons();
+
+            }
+        }.start();
+        mTimeRunning = true;
+        updateButtons();
+    }
+
+    private void pauseTimer(){
+        mCountDownTimer.cancel();
+        mTimeRunning = false;
+        updateButtons();
+    }
+
+    private void resetTimer(){
+        mTimeLeftInMillis = START_TIME_IN_MILLIS;
+        updateCountDownText();
+        updateButtons();
+
+    }
+
+    private void updateCountDownText(){
+        int minutes = (int)(mTimeLeftInMillis/1000) / 60;
+        int seconds =(int)(mTimeLeftInMillis/1000) % 60;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        mTextViewCountDown.setText(timeLeftFormatted);
+    }
+
+    private void updateButtons()
+    {
+        if(mTimeRunning)
+        {
+            mButtonReset.setVisibility(View.INVISIBLE);
+            mButtonStartPause.setText("Pause");
+        } else {
+            mButtonStartPause.setText("Start");
+
+            if(mTimeLeftInMillis < 1000) {
+                mButtonStartPause.setVisibility(View.INVISIBLE);
+            } else {
+                mButtonStartPause.setVisibility(View.VISIBLE);
+            }
+
+            if(mTimeLeftInMillis < START_TIME_IN_MILLIS) {
+                mButtonReset.setVisibility(View.VISIBLE);
+            } else {
+                mButtonReset.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong("millisLeft",mTimeLeftInMillis);
+        outState.putBoolean("timerRunning", mTimeRunning);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        mTimeLeftInMillis = savedInstanceState.getLong("millisLeft");
+        mTimeRunning = savedInstanceState.getBoolean("timerRunning");
+        updateCountDownText();
+        updateButtons();
+
+        if(mTimeRunning) {
+            startTimer();
+        }
+    }
+}
